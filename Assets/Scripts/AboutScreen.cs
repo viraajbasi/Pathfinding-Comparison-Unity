d1bc4a409b1c4eb6880ec598ed0f5f41cfe8aeb0
Viraaj Basi <viraajbasi@gmail.com>
2022-01-11 14:20:43 +0000

    finished about screen

--- Assets/Scripts/AboutScreen.cs
+++ Assets/Scripts/AboutScreen.cs
@@ -1,7 +1,41 @@
 using UnityEngine;
 using TMPro;
+using System.IO;
 
 public class AboutScreen : MonoBehaviour
 {
     public TMP_Text algorithmInformation;
+
+    public void DijkstraInfo()
+    {
+        ReadInfoAndShow("Dijkstra");
+    }
+    
+    public void AStarInfo()
+    {
+        ReadInfoAndShow("AStar");
+    }
+    
+    public void BellmanFordInfo()
+    {
+        ReadInfoAndShow("BellmanFord");
+    }
+    
+    public void RecursiveBacktrackerInfo()
+    {
+        ReadInfoAndShow("RecursiveBacktracker");
+    }
+    
+    public void KruskalInfo()
+    {
+        ReadInfoAndShow("Kruskal");
+    }
+    
+    private void ReadInfoAndShow(string filename)
+    {
+        string path = $"Assets/Resources/{filename}.txt";
+        StreamReader streamReader = new StreamReader(path);
+
+        algorithmInformation.text = streamReader.ReadToEnd();
+    }
 }
