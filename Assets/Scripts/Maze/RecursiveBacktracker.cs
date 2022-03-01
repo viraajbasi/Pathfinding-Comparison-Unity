a793aec094dc4adf7cb728f26adc5d22f16d4675
Viraaj Basi <viraajbasi@gmail.com>
2022-03-01 10:55:57 +0000

    cleanup

--- Assets/Scripts/Maze/RecursiveBacktracker.cs
+++ Assets/Scripts/Maze/RecursiveBacktracker.cs
@@ -31,60 +31,36 @@ namespace Maze
 					switch (rndNeigbour.Wall)
 					{
 						case SharedWall.Top:
-							maze[maze.FindIndex(a => a.Coordinates.X == current.X && a.Coordinates.Y == current.Y)]
-								.Top = false;
+							maze[maze.FindIndex(a => a.Coordinates.X == current.X && a.Coordinates.Y == current.Y)].Top = false;
 							break;
 						case SharedWall.Bottom:
-							maze[maze.FindIndex(a => a.Coordinates.X == current.X && a.Coordinates.Y == current.Y)]
-								.Bottom = false;
+							maze[maze.FindIndex(a => a.Coordinates.X == current.X && a.Coordinates.Y == current.Y)].Bottom = false;
 							break;
 						case SharedWall.Left:
-							maze[maze.FindIndex(a => a.Coordinates.X == current.X && a.Coordinates.Y == current.Y)]
-								.Left = false;
+							maze[maze.FindIndex(a => a.Coordinates.X == current.X && a.Coordinates.Y == current.Y)].Left = false;
 							break;
 						case SharedWall.Right:
-							maze[maze.FindIndex(a => a.Coordinates.X == current.X && a.Coordinates.Y == current.Y)]
-								.Right = false;
+							maze[maze.FindIndex(a => a.Coordinates.X == current.X && a.Coordinates.Y == current.Y)].Right = false;
 							break;
 					}
 
 					switch (rndNeigbour.Wall)
 					{
 						case SharedWall.Top:
-							maze[
-									maze.FindIndex(a =>
-										a.Coordinates.X == neighbourPosition.X &&
-										a.Coordinates.Y == neighbourPosition.Y)]
-								.Bottom = false;
+							maze[maze.FindIndex(a => a.Coordinates.X == neighbourPosition.X && a.Coordinates.Y == neighbourPosition.Y)].Bottom = false;
 							break;
 						case SharedWall.Bottom:
-							maze[
-									maze.FindIndex(a =>
-										a.Coordinates.X == neighbourPosition.X &&
-										a.Coordinates.Y == neighbourPosition.Y)]
-								.Top = false;
+							maze[maze.FindIndex(a => a.Coordinates.X == neighbourPosition.X && a.Coordinates.Y == neighbourPosition.Y)].Top = false;
 							break;
 						case SharedWall.Left:
-							maze[
-									maze.FindIndex(a =>
-										a.Coordinates.X == neighbourPosition.X &&
-										a.Coordinates.Y == neighbourPosition.Y)]
-								.Right = false;
+							maze[maze.FindIndex(a => a.Coordinates.X == neighbourPosition.X && a.Coordinates.Y == neighbourPosition.Y)].Right = false;
 							break;
 						case SharedWall.Right:
-							maze[
-									maze.FindIndex(a =>
-										a.Coordinates.X == neighbourPosition.X &&
-										a.Coordinates.Y == neighbourPosition.Y)]
-								.Left = false;
+							maze[maze.FindIndex(a => a.Coordinates.X == neighbourPosition.X && a.Coordinates.Y == neighbourPosition.Y)].Left = false;
 							break;
 					}
 
-					maze[
-							maze.FindIndex(a =>
-								a.Coordinates.X == neighbourPosition.X && a.Coordinates.Y == neighbourPosition.Y)]
-						.Visited =
-						true;
+					maze[maze.FindIndex(a => a.Coordinates.X == neighbourPosition.X && a.Coordinates.Y == neighbourPosition.Y)].Visited = true;
 					positionStack.Push(neighbourPosition);
 				}
 			}
@@ -99,8 +75,7 @@ namespace Maze
 
 			if (position.X > 0) // Left Wall
 			{
-				if (!maze[maze.FindIndex(a => a.Coordinates.X == position.X - 1 && a.Coordinates.Y == position.Y)]
-					    .Visited)
+				if (!maze[maze.FindIndex(a => a.Coordinates.X == position.X - 1 && a.Coordinates.Y == position.Y)].Visited)
 				{
 					list.Add(new Neighbour
 					{
@@ -116,8 +91,7 @@ namespace Maze
 
 			if (position.Y > 0) // Bottom Wall
 			{
-				if (!maze[maze.FindIndex(a => a.Coordinates.X == position.X && a.Coordinates.Y == position.Y - 1)]
-					    .Visited)
+				if (!maze[maze.FindIndex(a => a.Coordinates.X == position.X && a.Coordinates.Y == position.Y - 1)].Visited)
 				{
 					list.Add(new Neighbour
 					{
@@ -133,8 +107,7 @@ namespace Maze
 
 			if (position.Y < height - 1) // Top Wall
 			{
-				if (!maze[maze.FindIndex(a => a.Coordinates.X == position.X && a.Coordinates.Y == position.Y + 1)]
-					    .Visited)
+				if (!maze[maze.FindIndex(a => a.Coordinates.X == position.X && a.Coordinates.Y == position.Y + 1)].Visited)
 				{
 					list.Add(new Neighbour
 					{
@@ -150,8 +123,7 @@ namespace Maze
 
 			if (position.X < width - 1) // Right Wall
 			{
-				if (!maze[maze.FindIndex(a => a.Coordinates.X == position.X + 1 && a.Coordinates.Y == position.Y)]
-					    .Visited)
+				if (!maze[maze.FindIndex(a => a.Coordinates.X == position.X + 1 && a.Coordinates.Y == position.Y)].Visited)
 				{
 					list.Add(new Neighbour
 					{
