3fe219eeb6794bf4033a77c7ea3adbf5df9669be
Viraaj Basi <viraajbasi@gmail.com>
2022-03-05 18:45:58 +0000

    Fixed compilation errors

--- Assets/Scripts/Maze/Dijkstra.cs
+++ Assets/Scripts/Maze/Dijkstra.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Maze;
+//namespace Maze;
 
-public class Dijkstra : MonoBehaviour
+namespace Maze
 {
-    private List<MazeCell> _maze = Render.SortedMaze;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class Dijkstra : MonoBehaviour
     {
-        
-    }
+        private List<MazeCell> _maze = Render.SortedMaze;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
